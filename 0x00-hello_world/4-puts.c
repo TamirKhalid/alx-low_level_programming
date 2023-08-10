7203f6c1967b1cff1f388e591f57de528614ba53
TamirKhalid <tamir2khalid@gmail.com>
2023-08-10 12:37:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)/**
-* This function prints the string and a new line to the standard output.*/
+* This function prints the string and a new line to the standard output.
+*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
