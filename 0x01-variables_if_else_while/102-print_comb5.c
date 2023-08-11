b45518268797664f92e2cd6130d681d2d6abe64d
TamirKhalid <tamir2khalid@gmail.com>
2023-08-11 13:48:24 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
-/**
- * main - Prints all possible different combinations of three digits
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-int i, j, k;
-i = 0;
-while (i < 10)
+int i, j;
+for (i = 0; i < 100; i++)
 {
-j = i + 1;
-while (j < 10)
+for (j = i + 1; j < 100; j++)
 {
-k = j + 1;
-while (k < 10)
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(k + '0');
-if (i < 7 || j < 8 || k < 9)
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
-k++;
 }
-j++;
-}
-i++;
 }
 putchar('\n');
 return (0);
