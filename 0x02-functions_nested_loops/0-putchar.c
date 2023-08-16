44466124b68f89a997ea6b5206828062514edc8a
TamirKhalid <tamir2khalid@gmail.com>
2023-08-16 04:51:32 -0700

    puchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#ifndef main_h
-#define main_h
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
-int print_sign(int n);
-int_abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_times_table(int n);
-#endif
+#include "main.h"
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
