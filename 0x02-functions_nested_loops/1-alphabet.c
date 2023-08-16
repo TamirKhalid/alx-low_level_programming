2ec9378c355d6c5de2434f80d044a951ed4d5b68
TamirKhalid <tamir2khalid@gmail.com>
2023-08-16 05:09:10 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,10 @@
  */
 void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-_putchar(c);
-_putchar('\n');
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+        _putchar(c);
+    _putchar('\n');
 }
+
