1cd21e3de7d82221e81341475463b98748727c76
TamirKhalid <tamir2khalid@gmail.com>
2023-08-16 04:25:55 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-int main(void)
-{
-printf("_putchar\n");
-return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void print_hello(void);
+int add_two(int a, int b);
+/* ... */
+#endif /* MAIN_H */
