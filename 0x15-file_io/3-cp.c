5c13d80a2d4c955ae2ce16765008f8311391abc2
Tamir_Khalid <tamir2khalid@gmail.com>
2023-11-27 14:11:41 -0800

    3-cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 char *create_buffer(char *file);
 void close_file(int fd);
 
@@ -22,7 +21,6 @@ exit(99);
 }
 return (buffer);
 }
-
 /**
  *  close_file - Closes file descriptors.
  *  @fd: The file descriptor to be closed.
@@ -38,7 +36,6 @@ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 }
-
 /**
  *  main - Copies the contents of a file to another file.
  *  @argc: The number of arguments supplied to the program.
@@ -49,7 +46,6 @@ exit(100);
  *  If file_to cannot be created or written to - exit code 99.
  *  If file_to or file_from cannot be closed - exit code 100.
  */
-
 int main(int argc, char *argv[])
 {
 int from, to, r, w;
